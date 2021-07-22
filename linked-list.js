7a495d520dfd999aec633748c38e00ee46546f39
fan777 <fan777@gmail.com>
2021-07-22 14:20:49 -0700

    completed assignment

--- linked-list.js
+++ linked-list.js
@@ -18,58 +18,134 @@ class LinkedList {
     for (let val of vals) this.push(val);
   }
 
+  getNode(idx) {
+    if (idx < 0 || idx >= this.length)
+      throw new Error(`invalid index`);
+
+    let current = this.head;
+    for (let i = 0; i < idx; i++) {
+      current = current.next;
+    }
+    return current;
+  }
+
   /** push(val): add new value to end of list. */
 
   push(val) {
+    let newNode = new Node(val);
 
+    if (this.head === null)
+      this.head = newNode;
+    if (this.tail !== null)
+      this.tail.next = newNode;
+
+    this.tail = newNode;
+    this.length++;
   }
 
   /** unshift(val): add new value to start of list. */
 
   unshift(val) {
+    if (this.head === null)
+      this.push(val);
+    else {
+      let newNode = new Node(val);
+      newNode.next = this.head;
 
+      this.head = newNode;
+      this.length++;
+    }
   }
 
   /** pop(): return & remove last item. */
 
   pop() {
-
+    return this.removeAt(this.length - 1);
   }
 
   /** shift(): return & remove first item. */
 
   shift() {
-
+    return this.removeAt(0);
   }
 
   /** getAt(idx): get val at idx. */
 
   getAt(idx) {
-
+    let current = this.getNode(idx);
+    return current.val;
   }
 
   /** setAt(idx, val): set val at idx to val */
 
   setAt(idx, val) {
-
+    let current = this.getNode(idx);
+    current.val = val;
   }
 
   /** insertAt(idx, val): add node w/val before idx. */
 
   insertAt(idx, val) {
-
+    if (idx === 0)
+      this.unshift(val);
+    else if (idx === this.length) {
+      this.push(val);
+    } else {
+      let newNode = new Node(val);
+      let beforeNode = this.getNode(idx - 1);
+      let afterNode = beforeNode.next;
+      beforeNode.next = newNode;
+      newNode.next = afterNode;
+      this.length++;
+    }
   }
 
   /** removeAt(idx): return & remove item at idx, */
 
   removeAt(idx) {
+    let value;
+    let current;
 
+    if (this.length > 1 && idx > 0) {
+      // get previous node
+      current = this.getNode(idx - 1);
+      value = current.next.val;
+      current.next = null;
+    } else {
+      // get target node
+      current = this.getNode(idx);
+      value = current.val;
+    }
+
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      if (idx === this.length) {
+        this.tail = current;
+      }
+      if (idx === 0) {
+        this.head = current.next;
+      }
+    }
+    return value;
   }
 
   /** average(): return an average of all values in the list */
 
   average() {
-    
+    if (this.length <= 0)
+      return 0;
+
+    let current = this.head;
+    let sum = 0;
+
+    for (let i = 0; i < this.length; i++) {
+      sum += current.val;
+      current = current.next;
+    }
+    return (sum / this.length);
   }
 }
 
--- linked-list.test.js
+++ linked-list.test.js
@@ -1,7 +1,7 @@
 const LinkedList = require("./linked-list");
 
-describe("push", function() {
-  it("appends node and increments length", function() {
+describe("push", function () {
+  it("appends node and increments length", function () {
     let lst = new LinkedList();
 
     lst.push(5);
@@ -23,8 +23,8 @@ describe("push", function() {
   });
 });
 
-describe("unshift", function() {
-  it("adds node at start and increments length", function() {
+describe("unshift", function () {
+  it("adds node at start and increments length", function () {
     let lst = new LinkedList();
 
     lst.unshift(5);
@@ -46,8 +46,8 @@ describe("unshift", function() {
   });
 });
 
-describe("pop", function() {
-  it("removes node at end and decrements length", function() {
+describe("pop", function () {
+  it("removes node at end and decrements length", function () {
     let lst = new LinkedList([5, 10]);
 
     expect(lst.pop()).toBe(10);
@@ -59,11 +59,12 @@ describe("pop", function() {
     expect(lst.tail).toBe(null);
     expect(lst.head).toBe(null);
     expect(lst.length).toBe(0);
+    expect(() => { lst.pop(); }).toThrow("invalid index");
   });
 });
 
-describe("shift", function() {
-  it("removes node at start and decrements length", function() {
+describe("shift", function () {
+  it("removes node at start and decrements length", function () {
     let lst = new LinkedList([5, 10]);
 
     expect(lst.shift()).toBe(5);
@@ -77,17 +78,18 @@ describe("shift", function() {
   });
 });
 
-describe("getAt", function() {
-  it("gets val at index", function() {
+describe("getAt", function () {
+  it("gets val at index", function () {
     let lst = new LinkedList([5, 10]);
 
     expect(lst.getAt(0)).toBe(5);
     expect(lst.getAt(1)).toBe(10);
+    expect(() => { lst.getAt(2); }).toThrow("invalid index");
   });
 });
 
-describe("setAt", function() {
-  it("sets val at index", function() {
+describe("setAt", function () {
+  it("sets val at index", function () {
     let lst = new LinkedList([5, 10]);
 
     expect(lst.setAt(0, 1));
@@ -97,8 +99,8 @@ describe("setAt", function() {
   });
 });
 
-describe("insertAt", function() {
-  it("inserts node and adjusts nearby nodes", function() {
+describe("insertAt", function () {
+  it("inserts node and adjusts nearby nodes", function () {
     let lst = new LinkedList([5, 10, 15, 20]);
 
     lst.insertAt(2, 12);
@@ -114,7 +116,7 @@ describe("insertAt", function() {
     expect(lst.tail.val).toBe(25);
   });
 
-  it("inserts into empty list", function() {
+  it("inserts into empty list", function () {
     let lst = new LinkedList();
 
     lst.insertAt(0, 5);
@@ -124,8 +126,8 @@ describe("insertAt", function() {
   });
 });
 
-describe("removeAt", function() {
-  it("removes from 1-item list", function() {
+describe("removeAt", function () {
+  it("removes from 1-item list", function () {
     let lst = new LinkedList(["a"]);
 
     lst.removeAt(0);
@@ -135,13 +137,13 @@ describe("removeAt", function() {
   });
 });
 
-describe("average", function() {
-  it("calculates the average of items in a list", function() {
+describe("average", function () {
+  it("calculates the average of items in a list", function () {
     let lst = new LinkedList([2, 3, 1, 1, 7, 6, 9]);
     expect(lst.average()).toBeCloseTo(4.1429, 4);
   });
 
-  it("returns 0 for empty lists", function() {
+  it("returns 0 for empty lists", function () {
     let lst = new LinkedList();
     expect(lst.average()).toBe(0);
   });
